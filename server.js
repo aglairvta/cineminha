5da35db996c048415fdbf4cd52ba555a490014b6
aglairvta <aglairgb@hotmail.com>
2024-09-26 00:17:33 -0400

    up

--- server.js
+++ server.js
@@ -2,35 +2,27 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.static('public')); 
-app.get('/api/suggestions', async (req, res) => {
-    const query = req.query.query;
+
+app.get('/api/movies', async (req, res) => {
+    const { page = 1, query = '' } = req.query;
     const apiKey = process.env.API_KEY;
+
+    const url = `https://www.omdbapi.com/?s=${encodeURIComponent(query)}&page=${page}&apikey=${apiKey}`;
+
     try {
         const { default: fetch } = await import('node-fetch');
-        const response = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=${apiKey}`);
+        const response = await fetch(url);
         const data = await response.json();
-        res.json(data);
+        res.json(data.Response === 'True' ? data.Search : []);
     } catch (error) {
-        console.error('Error fetching suggestions:', error);
+        console.error('Error fetching movies:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-app.get('/api/content', async (req, res) => {
-    const query = req.query.query;
-    const apiKey = process.env.API_KEY;
-    try {
-        // Importa node-fetch dinamicamente
-        const { default: fetch } = await import('node-fetch');
-        const response = await fetch(`https://www.omdbapi.com/?t=${query}&apikey=${apiKey}`);
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        console.error('Error fetching content:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
